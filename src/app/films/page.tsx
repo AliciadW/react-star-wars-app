5ec03797a22a38f97768960d38dc59cc917e3af6
Alicia de Wet <alicia.dewet@gmail.com>
2024-12-13 15:34:34 +0000

    :lipstick: update layout and introduce film card component to films page

--- src/app/films/page.tsx
+++ src/app/films/page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import FilmCard from '@/app/components/FilmCard';
+
 export default async function Films() {
   const filmsData = await fetch('https://swapi.dev/api/films')
   const filmsResponse = await filmsData.json()
@@ -5,12 +8,16 @@ export default async function Films() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>FILMS</h1>
+      <div className="flex items-center">
+        <Image src='/icons8.svg' alt='' width={50} height={50} />
+        <h2 className='text-xl font-bold mx-3'>FILMS</h2>
+        <Image src='/icons8.svg' alt='' width={50} height={50} />
+      </div>
 
       <ul className="m-4">
         {films.map((film) => (
           <li key={film.episode_id}>
-            {film.title}
+            <FilmCard title={film.title} director={film.director} date={film.release_date} />
           </li>
         ))}
       </ul>
