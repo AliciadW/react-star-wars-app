e7d453910f43d2c11c7db70394dee1bd637db20a
Alicia de Wet <alicia.dewet@gmail.com>
2024-12-16 13:30:08 +0000

    :recycle: Implement routing for characters, planets, starships, vehicles, species

--- src/app/components/FilmDetails.tsx
+++ src/app/components/FilmDetails.tsx
@@ -1,13 +1,19 @@
+import { useRouter } from 'next/navigation';
+
 export default function FilmDetails({film}) {
+  const router =  useRouter()
+
   return (
     <div>
-      <p>Episode {film.episode_id}</p>
+      <p className="font-bold">Episode {film.episode_id}</p>
       <p className="mt-2">{film.opening_crawl}</p>
-      <p className="mt-2">View {film.characters.length} characters</p>
-      <p className="mt-2">View {film.planets.length} planets</p>
-      <p className="mt-2">View {film.species.length} species</p>
-      <p className="mt-2">View {film.starships.length} starships</p>
-      <p className="mt-2">View {film.vehicles.length} vehicles</p>
+      <div className="flex flex-col">
+        <button className="mt-2" onClick={() => router.push('/characters')}>View {film.characters.length} characters</button>
+        <button className="mt-2" onClick={() => router.push('/planets')}>View {film.planets.length} planets</button>
+        <button className="mt-2" onClick={() => router.push('/species')}>View {film.species.length} species</button>
+        <button className="mt-2" onClick={() => router.push('/starships')}>View {film.starships.length} starships</button>
+        <button className="mt-2" onClick={() => router.push('/vehicles')}>View {film.vehicles.length} vehicles</button>
+      </div>
     </div>
   )
 }
