d3af91c1fd588a8730fc62c812096fdc9d30d3d9
Alicia de Wet <alicia.dewet@gmail.com>
2024-12-16 15:34:08 +0000

    :lipstick: style characters page, add film name to page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,15 +1,57 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+import { useState, useEffect } from 'react'
+
 import Image from 'next/image';
+import CharacterCard from '@/app/components/CharacterCard';
 
 export default function Characters() {
+  const params = useSearchParams()
+  const filmId = params.get('film')
+  const [film, setFilm] = useState(null)
+  const [characterIds, setCharacterIds] = useState([])
+
+  useEffect(() => {
+    async function fetchFilm() {
+      const filmData = await fetch(`https://swapi.tech/api/films/${filmId}`)
+      const filmResponse = await filmData.json()
+
+      setFilm(filmResponse.result)
+    }
+
+    fetchFilm()
+  }, []);
+
+
+  if (!film) return <div>Loading...</div>
+
+  if (film.properties.characters) {
+    const filmCharacters = film.properties.characters
+
+    // loop through character urls, get ids for all
+    filmCharacters.forEach((characterUrl: string) => {
+      characterIds.push(characterUrl.split('/')[5])
+    })
+  }
+
+  // TODO: paginate; for now only get first 5
+
   return (
     <div className="flex flex-col items-center">
       <div className="flex items-center">
         <Image src="/icons8.svg" alt="" width={50} height={50}/>
-        <h2 className="text-xl font-bold mx-3">Characters</h2>
+        <h2 className="text-xl font-bold mx-3">Characters {film && `in ${film.properties.title}`}</h2>
         <Image src="/icons8.svg" alt="" width={50} height={50}/>
       </div>
 
-      <p className="text-lg font-bold my-8">Coming soon...</p>
+      <ul className="m-4">
+        {characterIds.slice(0, 5).map((characterId, index) => (
+          <li key={index}>
+            <CharacterCard characterId={characterId}/>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
