5bc47028e10f64780b664bc575a72b2f25224cbe
Alicia de Wet <alicia.dewet@gmail.com>
2024-12-16 13:59:22 +0000

    :lipstick: add some styling to planets page

--- src/app/planets/page.tsx
+++ src/app/planets/page.tsx
@@ -1,7 +1,13 @@
+import Image from 'next/image';
+
 export default function Films() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>PLANETS</h1>
+        <div className="flex items-center">
+          <Image src="/icons8.svg" alt="" width={50} height={50}/>
+          <h2 className="text-xl font-bold mx-3">PLANETS</h2>
+          <Image src="/icons8.svg" alt="" width={50} height={50}/>
+        </div>
     </div>
   )
 }
