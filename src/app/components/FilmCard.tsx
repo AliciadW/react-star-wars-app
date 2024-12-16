9382787224c0bb412b5253e35592675790cadda3
Alicia de Wet <alicia.dewet@gmail.com>
2024-12-16 12:45:15 +0000

    :art: update structure and format of code

--- src/app/components/FilmCard.tsx
+++ src/app/components/FilmCard.tsx
@@ -1,17 +1,16 @@
-export default function FilmCard({title, director, date}) {
+export default function FilmCard({ filmData, onCardClick }) {
   function displayDate(date) {
     return date?.split('-')[0] || ''
   }
 
   return (
-    <div className="flex flex-col m-3 p-2 cursor-pointer bg-cardBackground rounded-md hover:bg-cardBackgroundHover">
-      <h3 className='text-lg font-bold'>{title}</h3>
+    <div className="flex flex-col m-3 p-2 cursor-pointer bg-cardBackground rounded-md hover:bg-cardBackgroundHover" onClick={onCardClick}>
+      <h3 className="text-lg font-bold">{filmData.title}</h3>
 
       <div className="flex justify-between">
-        <p>{director}</p>
-        <p>{displayDate(date)}</p>
+        <p>{filmData.director}</p>
+        <p>{displayDate(filmData.release_date)}</p>
       </div>
-
     </div>
   )
 }
