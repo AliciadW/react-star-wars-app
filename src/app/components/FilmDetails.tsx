8ae6b361b789238150ae5bf9429e234a4a71840f
Alicia de Wet <alicia.dewet@gmail.com>
2024-12-16 15:32:46 +0000

    :poop: use query params to get film id for subsequent requests - will need to be updated to use state

--- src/app/components/FilmDetails.tsx
+++ src/app/components/FilmDetails.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/navigation';
 
-export default function FilmDetails({film}) {
+export default function FilmDetails({film, uid}) {
   const router =  useRouter()
 
   return (
@@ -8,8 +8,8 @@ export default function FilmDetails({film}) {
       <p className="font-bold">Episode {film.episode_id}</p>
       <p className="mt-2">{film.opening_crawl}</p>
       <div className="flex flex-col">
-        <button className="mt-2" onClick={() => router.push('/characters')}>View {film.characters.length} characters</button>
-        <button className="mt-2" onClick={() => router.push('/planets')}>View {film.planets.length} planets</button>
+        <button className="mt-2" onClick={() => router.push(`/characters?film=${uid}`)}>View {film.characters.length} characters</button>
+        <button className="mt-2" onClick={() => router.push(`/planets?film=${uid}`)}>View {film.planets.length} planets</button>
         <button className="mt-2" onClick={() => router.push('/species')}>View {film.species.length} species</button>
         <button className="mt-2" onClick={() => router.push('/starships')}>View {film.starships.length} starships</button>
         <button className="mt-2" onClick={() => router.push('/vehicles')}>View {film.vehicles.length} vehicles</button>
